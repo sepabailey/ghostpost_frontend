ead6370721335a7e553c0378f7f268651cc69002
Sean Bailey <sepabailey@gmail.com>
2020-06-26 16:22:02 -0400

    set up frontend. added buttons and click handlers. still not fully functional.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+Author: Sean Bailey
+Co-Authors: Koren Nyles, Chris Wilson
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      post: [],
+    };
+  }
+
+  // Peter helped with getting click handlers to work
+  handleClickAllPosts = (event) => {
+    fetch("http://localhost:8000/Post")
+      .then((res) => res.json())
+      .then((res) => this.setState({ post: res }));
+  };
+
+  handleClickBoast = (event) => {
+    fetch("http://127.0.0.1:8000/Post/boast/")
+      .then((res) => res.json())
+      .then((res) => this.setState({ post: res }));
+  };
+
+  handleClickRoast = (event) => {
+    fetch("http://127.0.0.1:8000/Post/roast/")
+      .then((res) => res.json())
+      .then((res) => this.setState({ post: res }));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Boast or Roast</h1>
+        <button onClick={this.handleClickAllPosts}>Home</button>
+        <button onClick={this.handleClickBoast}>Boasts</button>
+        <button onClick={this.handleClickRoast}>Roasts</button>
+        <button>Highest Votes</button>
+        {this.state.post.map((p) => {
+          return (
+            <ul>
+              <li>
+                Title: {p.post_title}
+                <br />
+                Date: {p.date}
+                <br />
+                Body: {p.body}
+                <br />
+                <button>UpVote{p.upvotes}</button>
+                <button>DownVote{p.downvotes}</button>
+              </li>
+            </ul>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
